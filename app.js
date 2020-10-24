7994bf66871cb0f42a8f50d22af9ab6891b126f7
Julio-P1 <70768415+Julio-P1@users.noreply.github.com>
2020-10-23 23:38:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+const container = document.querySelector('#container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+const monedaLocal = document.querySelector('#localCoin');
+const criptoMoneda = document.querySelector('#criptoCoin');
+
+window.addEventListener('load', () => {
+    traerTopTen();
+    formulario.addEventListener('submit', obtenerMonedas);
+});
+
+function traerTopTen () {
+
+    const apiKey = 'f3983db33bd720a5ee73b51c695dcb38b9837d343b74f2d42531f5e1b8f3112c';
+    const URL = `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&appid=${apiKey}`;
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(datos => {
+            console.log(datos);
+            /*console.log(datos.Data[0].CoinInfo.Name);
+            console.log(datos.Data[0].CoinInfo.FullName);*/
+            for (let i = 0; i < datos.Data.length; i++) {
+                //console.log(datos.Data[i].CoinInfo);
+                let criptoOpcion = document.createElement('option');
+                criptoOpcion.setAttribute('value', `${datos.Data[i].CoinInfo.Name}`)
+                criptoOpcion.textContent = `${datos.Data[i].CoinInfo.FullName}`;
+                criptoMoneda.appendChild(criptoOpcion);
+                //console.log(criptoOpcion);   
+            }
+        })
+}
+
+function obtenerMonedas (event) {
+    event.preventDefault();
+
+    let valorMoneda = monedaLocal.value;
+    let criptoValor = criptoMoneda.value;
+    console.log(valorMoneda);
+    console.log(criptoValor);
+
+    cotizarMoneda(valorMoneda, criptoValor);
+    
+}
+
+function cotizarMoneda (valorMoneda, criptoValor) {
+
+    const apiKey = 'f3983db33bd720a5ee73b51c695dcb38b9837d343b74f2d42531f5e1b8f3112c';
+    const URL = `https://min-api.cryptocompare.com/data/price?fsym=${criptoValor}&tsyms=${valorMoneda}&appid=${apiKey}`;
+    console.log(URL);
+    
+    fetch(URL)
+        .then(response => response.json())
+        .then(datos => {
+            console.log(datos);
+
+            const valorActual = document.createElement('p');
+            valorActual.innerHTML = ` 1 ${criptoValor} cuesta ${Object.values(datos).toString()} ${valorMoneda}`;
+
+            const divResultado = document.createElement('div');
+            divResultado.appendChild(valorActual);
+            
+            container.appendChild(divResultado);
+
+            valorActual.classList.add('show-value');
+
+            setTimeout(() => {
+                divResultado.remove();
+            }, 5000);
+
+        })
+}
